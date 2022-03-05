1f9967de654fe774d8ae498017a17a3e09f5567f
syaukhul <syaukhul@gmail.com>
2022-03-06 00:32:56 +0700

    update data

---
+++ handler/book.go
@@ -0,0 +1,62 @@
+package handler
+
+import (
+	"fmt"
+	"net/http"
+
+	"pustaka-api/book"
+
+	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
+)
+
+func RootHandler(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"name": "Muhammad Syaukhul A'la",
+		"bio":  "A Software Engineer at KodingWorks",
+	})
+}
+
+func HelloHandler(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"title":    "Hello World",
+		"subtitle": "Belajar Web Api Golang",
+	})
+}
+
+func BooksHandler(c *gin.Context) {
+	id := c.Param("id")
+	title := c.Param("title")
+
+	c.JSON(http.StatusOK, gin.H{"id": id, "title": title})
+}
+
+func QueryHandler(c *gin.Context) {
+	id := c.Query("title")
+	price := c.Query("price")
+
+	c.JSON(http.StatusOK, gin.H{"title": id, "price": price})
+}
+
+func PostBookHandler(c *gin.Context) {
+	var bookInput book.BookInput
+
+	err := c.ShouldBindJSON(&bookInput)
+	if err != nil {
+
+		errorMessages := []string{}
+		for _, e := range err.(validator.ValidationErrors) {
+			errorMessage := fmt.Sprintf("Error on filed %s, condition: %s", e.Field(), e.ActualTag())
+			errorMessages = append(errorMessages, errorMessage)
+		}
+		c.JSON(http.StatusBadRequest, gin.H{
+			"errors": errorMessages,
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"title": bookInput.Title,
+		"price": bookInput.Price,
+	})
+}
